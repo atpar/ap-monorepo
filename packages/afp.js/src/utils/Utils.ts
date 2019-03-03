587c6858e60b70d2158bfc51ab27cd6d37af3b66
jo-es <johannes.escherich+github@atpar.io>
2019-03-03 15:41:26 +0100

    Convert Utils class into separate exportable functions

--- packages/afp.js/src/utils/Utils.ts
+++ packages/afp.js/src/utils/Utils.ts
@@ -1,12 +1,9 @@
 import web3Utils from 'web3-utils';
 
+export function toHex (mixed: any) : string {
+  return web3Utils.toHex(mixed);
+}
 
-export class Utils {
-  public static toHex (mixed: any) : string {
-    return web3Utils.toHex(mixed);
-  }
-
-  public static hexToUtf8 (hex: string) : string {
-    return web3Utils.hexToUtf8(hex);
-  }
+export function hexToUtf8 (hex: string) : string {
+  return web3Utils.hexToUtf8(hex);
 }
