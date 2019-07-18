7e990d5a8d375c8e8a82e7373e462682d90b01d0
jo-es <johannes.escherich+github@atpar.io>
2019-07-18 18:47:13 +0200

    Remove ap-helper and update start.sh script

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@
 trap "exit" INT TERM
 trap "printf '\nshutdown ...\n' && kill 0" EXIT
 
-echo "running ganache-cli ..."
+echo "• running ganache-cli"
 
 { 
 	npx --quiet ganache-cli -i 1994 -t "2009-01-03T18:15:05" -e 5000000000 -d -m "helmet copy pause hood gun soon fork drum educate curious despair embrace"
@@ -13,41 +13,20 @@ sleep 1
 
 if [ ! -d "packages/ap-contracts/build" ]
 then
-	echo "compiling contracts ..."
+	echo "• compiling contracts"
 	(
 		cd packages/ap-contracts
 		npx --quiet truffle compile --all | 1>/dev/null
 	)
 fi
 
-echo "migrating contracts ..."
+echo "• migrating contracts"
 
 (
 	cd packages/ap-contracts
 	npx --quiet truffle migrate --reset --network development | 1>/dev/null
 )
 
-echo "clearing databases ..."
-
-(
-	cd packages/ap-helper
-	echo "" > Orderbook-Database.json
-)
-
-echo "running ap-helper ..."
-
-if ! [ -x "$(command -v yarn)" ]; then
-	{
-		cd packages/ap-helper
-		npm start
-	} 1>/dev/null &
-else
-	{
-		cd packages/ap-helper
-		yarn start
-	} 1>/dev/null &
-fi
-
-echo "ready ..."
+echo "✓ ready"
 
 while true; do sleep 1; done
