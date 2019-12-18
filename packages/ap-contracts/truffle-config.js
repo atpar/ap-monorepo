0e7a659d408c0fd0909452e84ca703386e38a986
jo-es <johannes.escherich+github@atpar.io>
2019-12-18 15:31:21 +0100

    Deploy ap-contracts and add addresses to deployments.json

--- packages/ap-contracts/truffle-config.js
+++ packages/ap-contracts/truffle-config.js
@@ -14,7 +14,8 @@ module.exports = {
       provider: () =>
         new HDWalletProvider(require('./mnemonic.js'), 'https://goerli.infura.io/v3/16b0bb612ec14abeb3617cff126ea5c0'),
       network_id: '5',
-      gas: 5000000
+      skipDryRun: true,
+      gas: 8000000
     },
     kovan: {
       provider: () =>
