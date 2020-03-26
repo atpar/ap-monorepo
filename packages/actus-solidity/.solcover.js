ee80e3ed4a6f559a96808cfedbb53afc82c0e739
jo-es <johannes.escherich+github@atpar.io>
2020-03-26 14:26:35 +0100

    Bump solidity-coverage and ignore files for coverage metering

--- packages/actus-solidity/.solcover.js
+++ packages/actus-solidity/.solcover.js
@@ -1,3 +1,6 @@
 module.exports = {
-  skipFiles: ['external/BokkyPooBah/BokkyPooBahsDateTimeLibrary.sol']
+  skipFiles: ['external/BokkyPooBah/BokkyPooBahsDateTimeLibrary.sol'],
+  skipFiles: ['test/TestPOF.sol'],
+  skipFiles: ['test/TestSTF.sol'],
+  skipFiles: ['Migrations.sol']
 };
--- packages/ap-contracts/.solcover.js
+++ packages/ap-contracts/.solcover.js
@@ -1,3 +1,11 @@
 module.exports = {
-  skipFiles: ['external/Dependencies.sol']
+  skipFiles: ['external/Dependencies.sol'],
+  skipFiles: ['token/SettlementToken.sol'],
+  skipFiles: ['token/VanillaFDT.sol'],
+  skipFiles: ['token/SimpleRestrictedFDT.sol'],
+  skipFiles: ['token/FDT/FundsDistributionToken.sol'],
+  skipFiles: ['token/FDT/IFundsDistributionToken.sol'],
+  skipFiles: ['token/FDT/SafeMathInt.sol'],
+  skipFiles: ['token/FDT/SafeMathUint.sol'],
+  skipFiles: ['Migrations.sol']
 };
