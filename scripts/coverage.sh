4d34852bec156aa9580d78f86e308c003a60a5fb
jo-es <johannes.escherich+github@atpar.io>
2020-03-26 21:57:37 +0100

    Merge coverage reports before piping them to coveralls

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,11 +1,14 @@
 (
   cd ./packages/ap-contracts
   yarn coverage
-  cat coverage/lcov.info | coveralls
+  # cat coverage/lcov.info | coveralls
 )
 
 (
   cd ./packages/actus-solidity
   yarn coverage
-  cat coverage/lcov.info | coveralls
+  # cat coverage/lcov.info | coveralls
 )
+
+./node_modules/.bin/lcov-result-merger 'packages/**/coverage/lcov.info' | coveralls
+
