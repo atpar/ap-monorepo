3d4c263a2bd19223f4d86f57083cf0d2a8c3c397
jo-es <johannes.escherich+github@atpar.io>
2020-04-28 14:57:49 +0200

    Disable allowUnlimitedContractSize in buidler configs

--- packages/actus-solidity/buidler.config.js
+++ packages/actus-solidity/buidler.config.js
@@ -13,7 +13,7 @@ module.exports = {
       url: 'http://127.0.0.1:8545'
     },
     buidlerevm: {
-      allowUnlimitedContractSize: true
+      allowUnlimitedContractSize: false
     }
   },
   solc: {
