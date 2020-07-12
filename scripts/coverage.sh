ee13c51a1e261c2850dd30e3c1c408d1c638ef58
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2020-07-13 02:35:56 +0300

    Revert "coverage script updated to test CI/CD build #3"
    This reverts commit 96d002c2

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,14 +4,14 @@ trap "rm -rf $mergerd_lconv" EXIT
 (
   cd ./packages/ap-contracts
   yarn coverage
-   cat coverage/lcov.info | coveralls
+  # cat coverage/lcov.info | coveralls
 )
 
 (
   cd ./packages/actus-solidity
   yarn coverage
-   cat coverage/lcov.info | coveralls
+  # cat coverage/lcov.info | coveralls
 )
 
-#./node_modules/.bin/lcov-result-merger 'packages/*/coverage/lcov.info' > "$mergerd_lconv"
-#./node_modules/.bin/coveralls < "$mergerd_lconv"
+./node_modules/.bin/lcov-result-merger 'packages/*/coverage/lcov.info' > "$mergerd_lconv"
+./node_modules/.bin/coveralls < "$mergerd_lconv"
