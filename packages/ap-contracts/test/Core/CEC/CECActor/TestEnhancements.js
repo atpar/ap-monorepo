c388dda228c4d6bfe08fcf87e0dcd8cd698cb0cb
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2020-08-26 16:37:17 +0300

    TestEnhancements refactored
    [skip ci]
    (#APm-45)

--- packages/ap-contracts/test/Core/CEC/CECActor/TestEnhancements.js
+++ packages/ap-contracts/test/Core/CEC/CECActor/TestEnhancements.js
@@ -1,65 +1,70 @@
+/*jslint node*/
+/*global before, beforeEach, describe, it, web3*/
+const assert = require('assert');
+const bre = require('@nomiclabs/buidler');
 const BigNumber = require('bignumber.js');
 
-const { setupTestEnvironment, getDefaultTerms, deployPaymentToken } = require('../../../helper/setupTestEnvironment');
-const { createSnapshot, revertToSnapshot, mineBlock } = require('../../../helper/blockchain')
+const { getSnapshotTaker, getDefaultTerms, deployPaymentToken } = require('../../../helper/setupTestEnvironment');
+const { mineBlock } = require('../../../helper/blockchain')
 const { decodeEvent } = require('../../../helper/scheduleUtils');
-const { generateSchedule, ZERO_ADDRESS, ZERO_BYTES32, ZERO_BYTES } = require('../../../helper/utils');
+const { generateSchedule, ZERO_ADDRESS, ZERO_BYTES32 } = require('../../../helper/utils');
 
 
-contract('CECActor', (accounts) => {
+describe('CECActor', () => {
 
-  const creatorObligor = accounts[1];
-  const creatorBeneficiary = accounts[2];
-  const counterpartyObligor = accounts[3];
-  const counterpartyBeneficiary = accounts[4];
-
-  let snapshot;
+  let actor, creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary;
 
   const getEventTime = async (_event, terms) => {
-    return Number(await this.PAMEngineInstance.computeEventTimeForEvent(
+    return Number(await this.PAMEngineInstance.methods.computeEventTimeForEvent(
       _event,
       terms.businessDayConvention,
       terms.calendar,
       terms.maturityDate
-    ));
+    ).call());
   }
 
-  before(async () => {
-    this.instances = await setupTestEnvironment(accounts);
-    Object.keys(this.instances).forEach((instance) => this[instance] = this.instances[instance]);
+  /** @param {any} self - `this` inside `before()` (and `it()`) */
+  const snapshotTaker = (self) => getSnapshotTaker(bre, self, async () => {
+    // code bellow runs right before the EVM snapshot gets taken
 
-    this.ownership = { creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary };
-    this.terms = { 
+    [
+      /*deployer*/, actor, creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary
+    ] = self.accounts;
+
+    self.ownership = { creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary };
+    self.terms = {
       ...await getDefaultTerms('PAM'),
       gracePeriod: { i: 1, p: 2, isSet: true },
       delinquencyPeriod: { i: 1, p: 3, isSet: true }
     };
 
     // deploy test ERC20 token
-    this.PaymentTokenInstance = await deployPaymentToken(creatorObligor, [counterpartyBeneficiary]);
+    self.PaymentTokenInstance = await deployPaymentToken(bre, creatorObligor, [counterpartyBeneficiary]);
     // set address of payment token as currency in terms
-    this.terms.currency = this.PaymentTokenInstance.address;
-    this.terms.settlementCurrency = this.PaymentTokenInstance.address;
-    this.terms.statusDate = this.terms.contractDealDate;
+    self.terms.currency = self.PaymentTokenInstance.options.address;
+    self.terms.settlementCurrency = self.PaymentTokenInstance.options.address;
+    self.terms.statusDate = self.terms.contractDealDate;
 
-    this.schedule = await generateSchedule(this.PAMEngineInstance, this.terms);
+    self.schedule = await generateSchedule(self.PAMEngineInstance, self.terms);
 
     // issue underlying
-    const tx = await this.PAMActorInstance.initialize(
-      this.terms,
-      this.schedule,
-      this.ownership,
-      this.PAMEngineInstance.address,
+    const tx = await self.PAMActorInstance.methods.initialize(
+      self.terms,
+      self.schedule,
+      self.ownership,
+      self.PAMEngineInstance.options.address,
       ZERO_ADDRESS
-    );  
-    
-    this.assetId = tx.logs[0].args.assetId;
+    ).send({ from: actor });
 
-    snapshot = await createSnapshot();
+    self.assetId = tx.events.InitializedAsset.returnValues.assetId;
   });
 
-  after(async () => {
-    await revertToSnapshot(snapshot);
+  before(async () => {
+    this.setupTestEnvironment = snapshotTaker(this);
+  });
+
+  beforeEach(async () => {
+    await this.setupTestEnvironment();
   });
 
   it('should trigger collateral', async () => {
@@ -74,105 +79,116 @@ contract('CECActor', (accounts) => {
     termsCEC.statusDate = this.terms.statusDate;
     // encode address of underlying in object of first contract reference
     termsCEC.contractReference_1.object = this.assetId;
-    termsCEC.contractReference_1.object2 = web3.utils.padLeft(this.PAMRegistryInstance.address, '64'); // workaround for solc bug (replace with bytes)
+    // workaround for solc bug (replace with bytes)
+    termsCEC.contractReference_1.object2 = web3.utils.padLeft(this.PAMRegistryInstance.options.address, '64');
     // encode collateral token address and collateral amount (notionalPrincipal of underlying + some over-collateralization)
-    const overCollateral = web3.utils.toWei('100').toString();
-    const collateralAmount = (new BigNumber(this.terms.notionalPrincipal)).plus(overCollateral);
+    const overCollateral = web3.utils.toWei('100');
+    const collateralAmount = (new BigNumber(this.terms.notionalPrincipal)).plus(overCollateral).toFixed();
     // encode collateralToken and collateralAmount in object of second contract reference
-    termsCEC.contractReference_2.object = await this.CECActorInstance.encodeCollateralAsObject(
-      this.PaymentTokenInstance.address,
+    termsCEC.contractReference_2.object = await this.CECActorInstance.methods.encodeCollateralAsObject(
+      this.PaymentTokenInstance.options.address,
       collateralAmount
-    );
-  
+    ).call();
+
     const scheduleCEC = await generateSchedule(this.CECEngineInstance, termsCEC);
 
     // counterparty has to set allowance == collateralAmount for custodian contract
-    await this.PaymentTokenInstance.approve(this.CustodianInstance.address, collateralAmount, { from: counterpartyBeneficiary });
+    await this.PaymentTokenInstance.methods.approve(this.CustodianInstance.options.address, collateralAmount)
+        .send({ from: counterpartyBeneficiary });
 
     // issue collateral enhancement
-    const txCEC = await this.CECActorInstance.initialize(
+    const { events } = await this.CECActorInstance.methods.initialize(
       termsCEC,
       scheduleCEC,
-      this.CECEngineInstance.address,
+      this.CECEngineInstance.options.address,
       ZERO_ADDRESS,
-      this.CustodianInstance.address,
-      this.PAMRegistryInstance.address
-    );
+      this.CustodianInstance.options.address,
+      this.PAMRegistryInstance.options.address
+    ).send({ from: actor });
 
-    const cecAssetId = txCEC.logs[0].args.assetId;
+    const cecAssetId = events.InitializedAsset.returnValues.assetId;
 
     // counterparty should have paid collateral
-    assert.equal(
-      (await this.PaymentTokenInstance.balanceOf(counterpartyBeneficiary)).toString(),
-      (new BigNumber(web3.utils.toWei('5000')).minus(collateralAmount)).toFixed()
+    assert.strictEqual(
+      (await this.PaymentTokenInstance.methods.balanceOf(counterpartyBeneficiary).call()),
+      (new BigNumber(web3.utils.toWei('5000')).minus(new BigNumber(collateralAmount))).toFixed()
     );
     // custodian should have received collateral
-    assert.equal(
-      (await this.PaymentTokenInstance.balanceOf(this.CustodianInstance.address)).toString(),
-      collateralAmount.toFixed()
+    assert.strictEqual(
+      (await this.PaymentTokenInstance.methods.balanceOf(this.CustodianInstance.options.address).call()),
+      collateralAmount
     );
 
     // settle IED
-    const iedEvent = await this.PAMRegistryInstance.getNextScheduledEvent(this.assetId);
-    const iedPayoff = await this.PAMEngineInstance.computePayoffForEvent(
+    const iedEvent = await this.PAMRegistryInstance.methods.getNextScheduledEvent(this.assetId).call();
+    const iedPayoff = await this.PAMEngineInstance.methods.computePayoffForEvent(
       this.terms,
-      await this.PAMRegistryInstance.getState(this.assetId),
+      await this.PAMRegistryInstance.methods.getState(this.assetId).call(),
       iedEvent,
       ZERO_BYTES32
-    );
+    ).call();
 
     // progress to schedule time of IED
     await mineBlock(Number(await getEventTime(iedEvent, this.terms)));
-    await this.PaymentTokenInstance.approve(this.PAMActorInstance.address, iedPayoff, { from: creatorObligor });
-    await this.PAMActorInstance.progress(this.assetId);
-    assert.equal(Number(decodeEvent(iedEvent).eventType), 2);
+    await this.PaymentTokenInstance.methods.approve(this.PAMActorInstance.options.address, iedPayoff)
+        .send({ from: creatorObligor });
+    await this.PAMActorInstance.methods.progress(this.assetId)
+        .send(this.txOpts);
+    assert.strictEqual(Number(decodeEvent(iedEvent).eventType), 2);
 
     // progress to schedule time of first IP (payoff == 0)
-    const ipEvent_1 = await this.PAMRegistryInstance.getNextScheduledEvent(this.assetId);
+    const ipEvent_1 = await this.PAMRegistryInstance.methods.getNextScheduledEvent(this.assetId).call();
     await mineBlock(Number(await getEventTime(ipEvent_1, this.terms)));
-    await this.PAMActorInstance.progress(this.assetId);
-    assert.equal(Number(decodeEvent(ipEvent_1).eventType), 9);
+    await this.PAMActorInstance.methods.progress(this.assetId)
+        .send(this.txOpts);
+    assert.strictEqual(Number(decodeEvent(ipEvent_1).eventType), 9);
 
     // progress to post-grace period of IP
-    const ipEvent_2 = await this.PAMRegistryInstance.getNextScheduledEvent(this.assetId);
+    const ipEvent_2 = await this.PAMRegistryInstance.methods.getNextScheduledEvent(this.assetId).call();
     await mineBlock(Number(await getEventTime(ipEvent_2, this.terms)) + 10000000);
-    await this.PAMActorInstance.progress(this.assetId);
-    assert.equal(Number(decodeEvent(ipEvent_2).eventType), 9);
+    await this.PAMActorInstance.methods.progress(this.assetId)
+        .send(this.txOpts);
+    assert.strictEqual(Number(decodeEvent(ipEvent_2).eventType), 9);
 
     // progress collateral enhancement
-    const xdEvent = await this.CECRegistryInstance.getNextUnderlyingEvent(web3.utils.toHex(cecAssetId));
+    const xdEvent = await this.CECRegistryInstance.methods
+        .getNextUnderlyingEvent(web3.utils.toHex(cecAssetId)).call();
     await mineBlock(Number(await getEventTime(xdEvent, termsCEC)));
-    await this.CECActorInstance.progress(web3.utils.toHex(cecAssetId));
-    assert.equal(Number(decodeEvent(xdEvent).eventType), 26);
+    await this.CECActorInstance.methods.progress(web3.utils.toHex(cecAssetId))
+        .send(this.txOpts);
+    assert.strictEqual(Number(decodeEvent(xdEvent).eventType), 26);
 
     // progress collateral enhancement
-    const stdEvent = await this.CECRegistryInstance.getNextUnderlyingEvent(web3.utils.toHex(cecAssetId));
+    const stdEvent = await this.CECRegistryInstance.methods
+        .getNextUnderlyingEvent(web3.utils.toHex(cecAssetId)).call();
     await mineBlock(Number(await getEventTime(stdEvent, termsCEC)));
-    await this.CECActorInstance.progress(web3.utils.toHex(cecAssetId));
-    assert.equal(Number(decodeEvent(stdEvent).eventType), 27);
+    await this.CECActorInstance.methods.progress(web3.utils.toHex(cecAssetId))
+        .send(this.txOpts);
+    assert.strictEqual(Number(decodeEvent(stdEvent).eventType), 27);
 
     // creator should have received seized collateral from custodian
-    assert.equal(
-      (await this.PaymentTokenInstance.balanceOf(creatorBeneficiary)).toString(),
+    assert.strictEqual(
+      (await this.PaymentTokenInstance.methods.balanceOf(creatorBeneficiary).call()),
       String(this.terms.notionalPrincipal)
     );
     // custodian should have not executed amount (overcollateral) left
-    assert.equal(
-      (await this.PaymentTokenInstance.balanceOf(this.CustodianInstance.address)).toString(),
-      overCollateral.toString()
+    assert.strictEqual(
+      (await this.PaymentTokenInstance.methods.balanceOf(this.CustodianInstance.options.address).call()),
+      overCollateral
     );
 
     // should return not executed amount to the counterparty (collateralizer)
-    await this.CustodianInstance.returnCollateral(cecAssetId);
+    await this.CustodianInstance.methods.returnCollateral(cecAssetId)
+        .send(this.txOpts);
 
     // custodian should have nothing left
-    assert.equal(
-      (await this.PaymentTokenInstance.balanceOf(this.CustodianInstance.address)).toString(),
+    assert.strictEqual(
+      (await this.PaymentTokenInstance.methods.balanceOf(this.CustodianInstance.options.address).call()),
       '0'
     );
     // counterparty (collateralizer) should have received not executed amount (overcollateral)
-    assert.equal(
-      (await this.PaymentTokenInstance.balanceOf(counterpartyBeneficiary)).toString(),
+    assert.strictEqual(
+      (await this.PaymentTokenInstance.methods.balanceOf(counterpartyBeneficiary).call()),
       web3.utils.toWei('5000')
     );
   });
