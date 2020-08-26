9d7575755be2945eed161cb7153491fad254a23f
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2020-08-26 17:07:45 +0300

    TestANNRegistry refactored
    [skip ci]
    (#APm-45)

--- packages/ap-contracts/test/Core/ANN/ANNRegistry/TestANNRegistry.js
+++ packages/ap-contracts/test/Core/ANN/ANNRegistry/TestANNRegistry.js
@@ -1,87 +1,94 @@
+/*jslint node*/
+/*global before, beforeEach, describe, it, web3*/
+const assert = require('assert');
+const bre = require('@nomiclabs/buidler');
 const { shouldFail } = require('openzeppelin-test-helpers');
 
-const { setupTestEnvironment } = require('../../../helper/setupTestEnvironment');
-const { generateSchedule, parseTerms, ZERO_BYTES32, ZERO_ADDRESS } = require('../../../helper/utils');
+const { getSnapshotTaker } = require('../../../helper/setupTestEnvironment');
+const { generateSchedule, parseTerms, ZERO_ADDRESS } = require('../../../helper/utils');
 
 const ASSET_ALREADY_EXISTS = 'ASSET_ALREADY_EXISTS';
 const UNAUTHORIZED_SENDER = 'UNAUTHORIZED_SENDER';
 
 
-contract('ANNRegistry', (accounts) => {
-  const actor = accounts[1];
+describe('ANNRegistry', () => {
+  let actor, creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary;
 
-  const creatorObligor = accounts[2];
-  const creatorBeneficiary = accounts[3];
-  const counterpartyObligor = accounts[4];
-  const counterpartyBeneficiary = accounts[5];
-  
+  /** @param {any} self - `this` inside `before()` (and `it()`) */
+  const snapshotTaker = (self) => getSnapshotTaker(bre, self, async () => {
+    // code bellow runs right before the EVM snapshot gets taken
+
+    [
+      /* deployer */, actor, creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary
+    ] = self.accounts;
+
+    self.assetId = 'ANN_01';
+    self.ownership = { creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary };
+    self.terms = require('../../../helper/terms/ANNTerms-complex.json');
+    self.schedule = await generateSchedule(self.ANNEngineInstance, self.terms);
+    self.state = await self.ANNEngineInstance.methods.computeInitialState(self.terms).call();
+  });
+
   before(async () => {
-    this.instances = await setupTestEnvironment(accounts);
-    Object.keys(this.instances).forEach((instance) => this[instance] = this.instances[instance]);
-
-    this.assetId = 'ANN_01';
-    this.ownership = { creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary };
-    this.terms = require('../../../helper/terms/ANNTerms-complex.json');
-    this.schedule = await generateSchedule(this.ANNEngineInstance, this.terms);
-    this.state = await this.ANNEngineInstance.computeInitialState(this.terms);
+    this.setupTestEnvironment = snapshotTaker(this);
+    await this.setupTestEnvironment();
   });
 
   it('should register an asset', async () => {
-    await this.ANNRegistryInstance.registerAsset(
+    await this.ANNRegistryInstance.methods.registerAsset(
       web3.utils.toHex(this.assetId),
       this.terms,
       this.state,
       this.schedule,
       this.ownership,
-      this.ANNEngineInstance.address,
+      this.ANNEngineInstance.options.address,
       actor,
       ZERO_ADDRESS
-    );
-    
-    const storedTerms = await this.ANNRegistryInstance.getTerms(web3.utils.toHex(this.assetId));
-    const storedState = await this.ANNRegistryInstance.getState(web3.utils.toHex(this.assetId));
-    const storedOwnership = await this.ANNRegistryInstance.getOwnership(web3.utils.toHex(this.assetId));
-    const storedEngineAddress = await this.ANNRegistryInstance.getEngine(web3.utils.toHex(this.assetId));
+    ).send({ from: actor });
 
-    assert.deepEqual(parseTerms(storedTerms), parseTerms(Object.values({ ...this.terms })));
-    assert.deepEqual(storedState, this.state);
-    assert.deepEqual(storedEngineAddress, this.ANNEngineInstance.address);
-    assert.equal(storedOwnership.creatorObligor, creatorObligor);
-    assert.equal(storedOwnership.creatorBeneficiary, creatorBeneficiary);
-    assert.equal(storedOwnership.counterpartyObligor, counterpartyObligor);
-    assert.equal(storedOwnership.counterpartyBeneficiary, counterpartyBeneficiary);
+    const storedTerms = await this.ANNRegistryInstance.methods.getTerms(web3.utils.toHex(this.assetId)).call();
+    const storedState = await this.ANNRegistryInstance.methods.getState(web3.utils.toHex(this.assetId)).call();
+    const storedOwnership = await this.ANNRegistryInstance.methods.getOwnership(web3.utils.toHex(this.assetId)).call();
+    const storedEngineAddress = await this.ANNRegistryInstance.methods.getEngine(web3.utils.toHex(this.assetId)).call();
+
+    assert.deepStrictEqual(parseTerms(storedTerms), parseTerms(Object.values({ ...this.terms })));
+    assert.deepStrictEqual(storedState, this.state);
+    assert.deepStrictEqual(storedEngineAddress, this.ANNEngineInstance.options.address);
+    assert.strictEqual(storedOwnership.creatorObligor, creatorObligor);
+    assert.strictEqual(storedOwnership.creatorBeneficiary, creatorBeneficiary);
+    assert.strictEqual(storedOwnership.counterpartyObligor, counterpartyObligor);
+    assert.strictEqual(storedOwnership.counterpartyBeneficiary, counterpartyBeneficiary);
   });
 
   it('should not overwrite an existing asset', async () => {
     await shouldFail.reverting.withMessage(
-      this.ANNRegistryInstance.registerAsset(
+      this.ANNRegistryInstance.methods.registerAsset(
         web3.utils.toHex(this.assetId),
         this.terms,
         this.state,
         this.schedule,
         this.ownership,
-        this.ANNEngineInstance.address,
+        this.ANNEngineInstance.options.address,
         actor,
         ZERO_ADDRESS
-      ),
+      ).send({ from: actor }),
       'BaseRegistry.setAsset: ' + ASSET_ALREADY_EXISTS
     );
   });
 
   it('should let the actor overwrite and update the terms, state of an asset', async () => {
-    await this.ANNRegistryInstance.setState(
-      web3.utils.toHex(this.assetId), 
+    await this.ANNRegistryInstance.methods.setState(
+      web3.utils.toHex(this.assetId),
       this.state,
-      { from: actor }
-    );
+    ).send({ from: actor });
   });
 
   it('should not let an unauthorized account overwrite and update the terms, state of an asset', async () => {
     await shouldFail.reverting.withMessage(
-      this.ANNRegistryInstance.setState(
-        web3.utils.toHex(this.assetId), 
+      this.ANNRegistryInstance.methods.setState(
+        web3.utils.toHex(this.assetId),
         this.state,
-      ),
+      ).send(this.txOpts),
       'AccessControl.isAuthorized: ' + UNAUTHORIZED_SENDER
     );
   });
