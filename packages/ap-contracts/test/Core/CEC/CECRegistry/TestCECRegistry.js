927074c47d16feb3aa01cd0989998e6a42baf88f
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2020-08-26 14:50:57 +0300

    TestCECRegistry refactored
    [skip ci]
    (#APm-45)

--- packages/ap-contracts/test/Core/CEC/CECRegistry/TestCECRegistry.js
+++ packages/ap-contracts/test/Core/CEC/CECRegistry/TestCECRegistry.js
@@ -1,51 +1,58 @@
+/*jslint node*/
+/*global before, beforeEach, describe, it, web3*/
+const assert = require('assert');
+const bre = require('@nomiclabs/buidler');
 const { shouldFail } = require('openzeppelin-test-helpers');
 
-const { setupTestEnvironment } = require('../../../helper/setupTestEnvironment');
-const { generateSchedule, parseTerms, ZERO_BYTES32, ZERO_ADDRESS } = require('../../../helper/utils');
+const { getSnapshotTaker } = require('../../../helper/setupTestEnvironment');
+const { generateSchedule, parseTerms, ZERO_ADDRESS } = require('../../../helper/utils');
 
 const ASSET_ALREADY_EXISTS = 'ASSET_ALREADY_EXISTS';
 const UNAUTHORIZED_SENDER = 'UNAUTHORIZED_SENDER';
 
 
-contract('CECRegistry', (accounts) => {
-  const actor = accounts[1];
+describe('CECRegistry', () => {
+  let actor, creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary, nobody;
 
-  const creatorObligor = accounts[2];
-  const creatorBeneficiary = accounts[3];
-  const counterpartyObligor = accounts[4];
-  const counterpartyBeneficiary = accounts[5];
-  
+  /** @param {any} self - `this` inside `before()` (and `it()`) */
+  const snapshotTaker = (self) => getSnapshotTaker(bre, self, async () => {
+    // code bellow runs right before the EVM snapshot gets taken
+
+    [
+        /* deployer */, actor, creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary, nobody
+    ] = self.accounts;
+    self.assetId = 'CEC_01';
+    self.ownership = { creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary };
+    self.terms = require('../../../helper/terms/CECTerms-complex.json');
+    self.schedule = await generateSchedule(self.CECEngineInstance, self.terms);
+    self.state = await self.CECEngineInstance.methods.computeInitialState(self.terms).call();
+  });
+
   before(async () => {
-    this.instances = await setupTestEnvironment(accounts);
-    Object.keys(this.instances).forEach((instance) => this[instance] = this.instances[instance]);
-
-    this.assetId = 'CEC_01';
-    this.ownership = { creatorObligor, creatorBeneficiary, counterpartyObligor, counterpartyBeneficiary };
-    this.terms = require('../../../helper/terms/CECTerms-complex.json');
-    this.schedule = await generateSchedule(this.CECEngineInstance, this.terms);
-    this.state = await this.CECEngineInstance.computeInitialState(this.terms);
+    this.setupTestEnvironment = snapshotTaker(this);
+    await this.setupTestEnvironment();
   });
 
   it('should register an asset', async () => {
-    await this.CECRegistryInstance.registerAsset(
+    await this.CECRegistryInstance.methods.registerAsset(
       web3.utils.toHex(this.assetId),
       this.terms,
       this.state,
       this.schedule,
       this.ownership,
-      this.CECEngineInstance.address,
+      this.CECEngineInstance.options.address,
       actor,
       ZERO_ADDRESS
-    );
-    
-    const storedTerms = await this.CECRegistryInstance.getTerms(web3.utils.toHex(this.assetId));
-    const storedState = await this.CECRegistryInstance.getState(web3.utils.toHex(this.assetId));
-    const storedOwnership = await this.CECRegistryInstance.getOwnership(web3.utils.toHex(this.assetId));
-    const storedEngineAddress = await this.CECRegistryInstance.getEngine(web3.utils.toHex(this.assetId));
+    ).send({from: actor});
+
+    const storedTerms = await this.CECRegistryInstance.methods.getTerms(web3.utils.toHex(this.assetId)).call();
+    const storedState = await this.CECRegistryInstance.methods.getState(web3.utils.toHex(this.assetId)).call();
+    const storedOwnership = await this.CECRegistryInstance.methods.getOwnership(web3.utils.toHex(this.assetId)).call();
+    const storedEngineAddress = await this.CECRegistryInstance.methods.getEngine(web3.utils.toHex(this.assetId)).call();
 
     assert.deepEqual(parseTerms(storedTerms), parseTerms(Object.values({ ...this.terms })));
     assert.deepEqual(storedState, this.state);
-    assert.deepEqual(storedEngineAddress, this.CECEngineInstance.address);
+    assert.deepEqual(storedEngineAddress, this.CECEngineInstance.options.address);
     assert.equal(storedOwnership.creatorObligor, creatorObligor);
     assert.equal(storedOwnership.creatorBeneficiary, creatorBeneficiary);
     assert.equal(storedOwnership.counterpartyObligor, counterpartyObligor);
@@ -54,34 +61,33 @@ contract('CECRegistry', (accounts) => {
 
   it('should not overwrite an existing asset', async () => {
     await shouldFail.reverting.withMessage(
-      this.CECRegistryInstance.registerAsset(
+      this.CECRegistryInstance.methods.registerAsset(
         web3.utils.toHex(this.assetId),
         this.terms,
         this.state,
         this.schedule,
         this.ownership,
-        this.CECEngineInstance.address,
+        this.CECEngineInstance.options.address,
         actor,
         ZERO_ADDRESS
-      ),
+      ).send({ from: actor }),
       'BaseRegistry.setAsset: ' + ASSET_ALREADY_EXISTS
     );
   });
 
   it('should let the actor overwrite and update the terms, state of an asset', async () => {
-    await this.CECRegistryInstance.setState(
-      web3.utils.toHex(this.assetId), 
+    await this.CECRegistryInstance.methods.setState(
+      web3.utils.toHex(this.assetId),
       this.state,
-      { from: actor }
-    );
+    ).send({ from: actor });
   });
 
   it('should not let an unauthorized account overwrite and update the terms, state of an asset', async () => {
     await shouldFail.reverting.withMessage(
-      this.CECRegistryInstance.setState(
-        web3.utils.toHex(this.assetId), 
+      this.CECRegistryInstance.methods.setState(
+        web3.utils.toHex(this.assetId),
         this.state,
-      ),
+      ).send({ from: nobody }),
       'AccessControl.isAuthorized: ' + UNAUTHORIZED_SENDER
     );
   });
