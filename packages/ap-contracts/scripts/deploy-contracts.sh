078ae11cc57596ce73446219cacfeb2844084fd0
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2020-08-31 18:24:46 +0300

    --restart option added to 'yarn migrate:..' in 'ap-contacts'
    (#APm-45)

--- packages/ap-contracts/scripts/deploy-contracts.sh
+++ packages/ap-contracts/scripts/deploy-contracts.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Usage:
+# $ deploy-contracts.sh <network [--terminate-on-rejections]>
 
 set -o errexit
 
@@ -11,16 +13,31 @@ grep -Eq '^(ap\-chain|goerli|kovan|rinkeby|ropsten)$' <<< "${network}" || {
   exit 1
 }
 
+# by default, run migration once (do not restart if failed)
+_restart=
+tags="migrate"
+if [ "$2" == "--restart" ]; then
+  # restart migration if an unhandled rejection occurs
+  _restart=yes
+  tags="migrate-terminate"
+fi
+
 echo "Deploying to ${network} ..."
 while test -f "${flag}"; do
-  node --unhandled-rejections=strict --max-old-space-size=4096 node_modules/.bin/buidler deploy \
+
+  node --max-old-space-size=4096 node_modules/.bin/buidler deploy \
     --network "${network}" \
-    --tags migrate \
+    --tags "${tags}" \
     --write true \
   && {
     echo "DONE successfully"
     exit 0
   }
+
+ [ -z "${_restart}" ] && {
+    echo "[!] FAILED"
+    exit 1
+ }
 
   echo "failed. running again ..."
   sleep 10
