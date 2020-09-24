5bedb84b62aee24038bba27e83f5232e0eabc627
jo-es <johannes.escherich+github@atpar.io>
2020-09-24 17:26:18 +0200

    Add wrapper contract

---
+++ packages/ap-contracts/contracts/FDT/CMTA20FDT.sol
@@ -0,0 +1,25 @@
+// "SPDX-License-Identifier: Apache-2.0"
+pragma solidity ^0.6.11;
+
+import "./ProxySafeCMTA20FDT.sol";
+
+/**
+ * @notice This contract, unlike its parent contract, is entirely instantiated by the `constructor`.
+ * Therefore this contract may NOT be used with a proxy that `delegatecall`s it.
+ */
+contract CMTA20FDT is ProxySafeCMTA20FDT {
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        IERC20 _fundsToken,
+        address owner,
+        uint256 initialAmount
+    )
+        public
+        ProxySafeCMTA20FDT()
+    {
+        initialize(name, symbol, _fundsToken, owner, initialAmount);
+    }
+
+}
