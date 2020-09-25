400a6af5e459ecd957712845e14862de1c6df027
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2020-09-25 23:53:40 +0300

    WIP: STKPOF drafted (#ap56)

---
+++ packages/actus-solidity/contracts/Engines/STK/STKPOF.sol
@@ -0,0 +1,89 @@
+// "SPDX-License-Identifier: Apache-2.0"
+pragma solidity ^0.6.11;
+pragma experimental ABIEncoderV2;
+
+import "../../Core/Core.sol";
+
+
+/**
+ * @title POF
+ * @notice Contains all Payoff Functions (POFs) for STK contracts
+ */
+contract STKPOF is Core {
+
+    /**
+     * Payoff Function for STK initial exchange
+     * @return the payoff at initial exchange
+     */
+    function POF_STK_IED (
+        STKTerms memory terms,
+        State memory state,
+        uint256 /* scheduleTime */,
+        bytes32 /* externalData */
+    )
+        internal
+        pure
+        returns(int256)
+    {
+        return (
+            roleSign(terms.contractRole) * state.quantity.floatMult(terms.issuePrice)
+        );
+    }
+
+    /**
+     * Payoff Function for STK dividend payment day
+     * @return the dividend payoff
+     */
+    function POF_STK_DPD (
+        STKTerms memory terms,
+        State memory state,
+        uint256 /* scheduleTime */,
+        bytes32 /* externalData */
+    )
+        internal
+        pure
+        returns(int256)
+    {
+        return (
+            roleSign(terms.contractRole) * state.quantity.floatMult(state.couponAmountFixed)
+        );
+    }
+
+    /**
+     * Payoff Function for STK Redemption Payment Day
+     * @return the redemption payoff
+     */
+    function POF_STK_RPD (
+        STKTerms memory terms,
+        State memory state,
+        uint256 /* scheduleTime */,
+        bytes32 /* externalData */
+    )
+        internal
+        pure
+        returns(int256)
+    {
+        return (
+            roleSign(terms.contractRole) * state.exerciseQuantity.floatMult(state.exerciseAmount)
+        );
+    }
+
+     /**
+     * Payoff Function for STK termination events
+     * @return the termination payoff
+     */
+    function POF_STK_TD (
+        STKTerms memory terms,
+        State memory state,
+        uint256 /* scheduleTime */,
+        bytes32 /* externalData */
+    )
+        internal
+        pure
+        returns(int256)
+    {
+        return (
+            roleSign(terms.contractRole) * state.quantity.floatMult(state.exerciseAmount)
+        );
+    }
+}
