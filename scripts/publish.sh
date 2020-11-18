aa2192a9301f0e868adddf6a4f8323b0dbccc48f
jo-es <johannes.escherich+github@atpar.io>
2020-11-18 17:00:11 +0100

    Fix publishing script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ lerna run build
 # create tag and push commit
 lerna version
 # publish to GitHub Packages Registry
-lerna publish --registry https://registry.npmjs.org/
+lerna publish from-package --registry https://registry.npmjs.org/
 # publish to NPM Registry
-lerna publish --registry https://npm.pkg.github.com/
+lerna publish from-package --registry https://npm.pkg.github.com/
 
