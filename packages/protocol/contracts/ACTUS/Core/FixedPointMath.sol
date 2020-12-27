b1e3c88d09c564d0b32457594f0aab91b4fafb0d
jo-es <johannes.escherich+github@atpar.io>
2020-12-27 14:37:18 +0100

    Rename file and format code

--- packages/protocol/contracts/ACTUS/Core/FixedPointMath.sol
+++ packages/protocol/contracts/ACTUS/Core/FixedPointMath.sol
@@ -18,39 +18,55 @@ library FixedPointMath {
      * as devision (normalization) is performed after multiplication
      * Upper boundary would be (10 ** 58) * (MULTIPLICATOR) == ~10 ** 76
      */
-    function fixedMul(int256 a, int256 b)
-        internal
-        pure
-        returns (int256)
-    {
+    function fixedMul(int256 a, int256 b) internal pure returns (int256) {
         if (a == 0 || b == 0) return 0;
 
-        require(!(a == -1 && b == INT256_MIN), "FixedPointMath.fixedMul: OVERFLOW_DETECTED");
+        require(
+            !(a == -1 && b == INT256_MIN),
+            "FixedPointMath.fixedMul: OVERFLOW_DETECTED"
+        );
+
         int256 c = a * b;
-        require(c / a == b, "FixedPointMath.fixedMul: OVERFLOW_DETECTED");
+        require(
+            c / a == b,
+            "FixedPointMath.fixedMul: OVERFLOW_DETECTED"
+        );
 
         // normalize (divide by MULTIPLICATOR)
         int256 d = c / int256(MULTIPLICATOR);
-        require(d != 0, "FixedPointMath.fixedMul: CANNOT_REPRESENT_GRANULARITY");
+        require(
+            d != 0,
+            "FixedPointMath.fixedMul: CANNOT_REPRESENT_GRANULARITY"
+        );
 
         return d;
     }
 
-    function fixedDiv(int256 a, int256 b)
-        internal
-        pure
-        returns (int256)
-    {
-        require(b != 0, "FixedPointMath.fixedDiv: DIVIDED_BY_ZERO");
+    function fixedDiv(int256 a, int256 b) internal pure returns (int256) {
+        require(
+            b != 0,
+            "FixedPointMath.fixedDiv: DIVIDED_BY_ZERO"
+        );
+
+        if (a == 0) return 0;
 
         // normalize (multiply by MULTIPLICATOR)
-        if (a == 0) return 0;
         int256 c = a * int256(MULTIPLICATOR);
-        require(c / a == int256(MULTIPLICATOR), "FixedPointMath.fixedDiv: OVERFLOW_DETECTED");
+        require(
+            c / a == int256(MULTIPLICATOR),
+            "FixedPointMath.fixedDiv: OVERFLOW_DETECTED"
+        );
 
-        require(!(b == -1 && a == INT256_MIN), "FixedPointMath.fixedDiv: OVERFLOW_DETECTED");
+        require(
+            !(b == -1 && a == INT256_MIN),
+            "FixedPointMath.fixedDiv: OVERFLOW_DETECTED"
+        );
+
         int256 d = c / b;
-        require(d != 0, "FixedPointMath.fixedDiv: CANNOT_REPRESENT_GRANULARITY");
+        require(
+            d != 0,
+            "FixedPointMath.fixedDiv: CANNOT_REPRESENT_GRANULARITY"
+        );
 
         return d;
     }
@@ -59,13 +75,13 @@ library FixedPointMath {
      * @dev Returns the smallest of two signed numbers.
      */
     function min(int256 a, int256 b) internal pure returns (int256) {
-            return a <= b ? a : b;
+        return a <= b ? a : b;
     }
 
     /**
      * @dev Returns the largest of two signed numbers.
      */
     function max(int256 a, int256 b) internal pure returns (int256) {
-            return a >= b ? a : b;
+        return a >= b ? a : b;
     }
 }
